ccda1d57980bfa1cdc0ec209a6dbee023eed3b36
loco <vicolinho@gmail.com>
2012-01-05 15:46:54 +0100

    tuning templates add a new package templates which include a interface BasicTemplates, which contains basic templates like year or the different author names The TemplateEntity class which wrap the Pattern.class The AuhtorTemplateEntity extends the TemplateEntity class implement the concatentation method and the multi variant method to get mor precise templates to find a reference The TemplateBuilder class organize all used templates in a hashmap and can merge two templateEntity

---
+++ src/de/uni/leipzig/asv/zitationsgraph/extraction/templates/TemplateBuilder.java
@@ -0,0 +1,58 @@
+package de.uni.leipzig.asv.zitationsgraph.extraction.templates;
+
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
+public class TemplateBuilder {
+
+	
+	private HashMap<String,TemplateEntity> templateMap;
+	
+	public TemplateBuilder()
+	{
+		templateMap = new HashMap<String,TemplateEntity>();
+	}
+
+	/**
+	 * @param templateMap the templateMap to set
+	 */
+	public void addTemplate(String name ,TemplateEntity te) {
+		this.templateMap.put(name, te);
+	}
+
+	/**
+	 * @return the templateMap
+	 */
+	public TemplateEntity getTemplate(String name) {
+		return templateMap.get(name);
+	}
+	
+	public void resetBuilder(){
+		this.templateMap.clear();
+	}
+	
+	public void mergeTemplates (int tolerance,boolean lineBegin,boolean lineEnd,String regexSep,
+			String name,boolean optional, String name2,boolean optional2,String targetName){
+		TemplateEntity te1 = this.templateMap.get(name);
+		TemplateEntity te2 = this.templateMap.get(name2);
+		StringBuffer sb = new StringBuffer();
+			
+				sb.append(((lineBegin)?"^":"")+te1.getTemplate()+
+						((optional&&!lineBegin)?"?":"")+regexSep+".{0,"+tolerance+"}?"
+						+te2.getTemplate()+((lineEnd)?".{0,3}":"")+((optional2)?"?":""+((lineEnd)?"$":"")));
+		Pattern p = Pattern.compile(sb.toString(),Pattern.MULTILINE|Pattern.DOTALL);
+		this.templateMap.put(targetName, new TemplateEntity(p));
+	}
+	
+	public void mergeTemplates (int tolerance,boolean lineBegin,boolean lineEnd,String regexSep,
+			TemplateEntity te1,boolean optional, TemplateEntity te2,boolean optional2,String targetName){
+		
+		StringBuffer sb = new StringBuffer();
+			
+				sb.append(((lineBegin)?"^":"")+te1.getTemplate()+
+						((optional)?"?":"")+regexSep+".{0,"+tolerance+"}?"
+						+te2.getTemplate()+((lineEnd)?".{0,3}":"")+((optional2)?"?":""+((lineEnd)?"$":"")));
+		Pattern p = Pattern.compile(sb.toString(),Pattern.MULTILINE|Pattern.DOTALL);
+		this.templateMap.put(targetName, new TemplateEntity(p));
+	}
+}
